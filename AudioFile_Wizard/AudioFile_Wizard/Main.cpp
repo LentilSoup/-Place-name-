be83afa7cbda3dcb366d1ef6fe7c767f1337912e
linthacus <linthacus@gmail.com>
2017-08-20 15:51:56 +0100

    goyim it don't work

--- AudioFile_Wizard/AudioFile_Wizard/Main.cpp
+++ AudioFile_Wizard/AudioFile_Wizard/Main.cpp
@@ -1,9 +1,18 @@
 #include "Main.h"
 
 using namespace std;
+using namespace experimental::filesystem;
+
+
+
 
 int main() {
+	string PATH;
+
 	cout << "AudioFile Wizard Started" << endl;
+	cout << "Input file path" << endl;
+	cout << "Example: C:\Program Files\Java\n:" << endl;
+	cin >> PATH;
 
 
 	return 0;
