53a881aa720d3755434e9c93d37fa158d0374f57
linthacus <linthacus@gmail.com>
2017-08-20 13:04:50 +0100

    wing wong

--- AudioFile_Wizard/AudioFile_Wizard/Main.h
+++ AudioFile_Wizard/AudioFile_Wizard/Main.h
@@ -3,11 +3,30 @@
 
 using namespace std::experimental::filesystem;
 
-class file{
+class fileResource {
+public:
 
 
 
+};
 
+
+class fileEditor: public fileResource {
+public:
+	static void icoEdit()
+	{
+		
+	};
+
+	void timeStampChange()
+	{
+		
+	};
+
+	void FormatConvert(std::string path)
+	{
+		
+	};
 
 
 
